aff0c831c10be67863afa485cbc33011083149c6
Edgard Antonio Espinoza Martínez <edgardespinoza6@hotmail.com>
2013-04-22 21:24:21 -0600

    Cambio de base de datos

--- app/Config/database.php
+++ app/Config/database.php
@@ -61,7 +61,7 @@ class DATABASE_CONFIG {
 		'host' => 'localhost',
 		'login' => 'root',
 		'password' => '',
-		'database' => 'radionicaragua',
+		'database' => 'radio',
 		'prefix' => '',
 		'encoding' => 'utf8',
 	);
